1b53c0166d692e02563f4b0e8785d1bf744cf430
Vedant Patil <vedant.waman.patil@gmail.com>
2025-12-12 00:41:25 -0500

    Abstracted the hitting algorithm and added object side differentiation

---
+++ src/sphere.rs
@@ -0,0 +1,44 @@
+use crate::hittable::{HitRecord, Hittable};
+use crate::ray::Ray;
+use crate::vec3::{self, Point3};
+
+pub struct Sphere {
+    center: Point3,
+    radius: f64,
+}
+
+impl Sphere {
+    pub fn new(center: Point3, radius: f64) -> Sphere {
+        Sphere { center, radius }
+    }
+}
+
+impl Hittable for Sphere {
+    fn hit(&self, r: &Ray, t_min: f64, t_max: f64, rec: &mut HitRecord) -> bool {
+        let oc = r.origin() - self.center;
+        let a = r.direction().length_squared();
+        let half_b = vec3::dot(oc, r.direction());
+        let c = oc.length_squared() - self.radius * self.radius;
+        let discriminat = half_b * half_b - a * c;
+        if discriminat < 0.0 {
+            return false;
+        }
+
+        let sqrt_d = f64::sqrt(discriminat);
+
+        // Find the nearest root in range
+        let mut root = (-half_b - sqrt_d) / a;
+        if root <= t_min || t_max <= root {
+            root = (-half_b + sqrt_d) / a;
+
+            if root <= t_min || t_max <= root {
+                return false;
+            }
+        }
+        rec.t = root;
+        rec.p = r.at(rec.t);
+        let outward_normal = (rec.p - self.center) / self.radius;
+        rec.set_face_normal(r, outward_normal);
+        true
+    }
+}
